a2651a9d7412bca72ba82821435144829ddff156
Samueljain26 <samueljain77@gmail.com>
2025-02-25 11:39:33 +0530

    priniting a 2nd message

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,5 +1,5 @@
 class FirstJavaProgram{
-   public static void main(String args[][]){
+   public static void main(String[] arg){
       System.out.print("Starting with Capgemini Training");
    }
 }
