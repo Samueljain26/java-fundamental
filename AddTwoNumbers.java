33d288868abddb7c79c677e6a4e8fd66211dfb8c
Samueljain26 <samueljain77@gmail.com>
2025-02-27 13:42:42 +0530

    adding two numbers

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,10 +1,11 @@
 import java.util.*;
+
 class addTwoNumbers{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter First Number: ");
 		int x= sc.nextInt();
 			System.out.print("Enter Second Number: ");
- int y=sc.nextInt();
- System.out.print("sum=" + (x+y));
+		 int y=sc.nextInt();
+ 			System.out.print("sum=" + (x+y));
 }}
