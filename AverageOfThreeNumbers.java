104a7fbf5a93bb5173f1079d9a0eaa57aee044b1
Samueljain26 <samueljain77@gmail.com>
2025-02-27 13:17:21 +0530

    calculating Average of Three Numbers

---
+++ AverageOfThreeNumbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class AverageOfThreeNumbers{	
+         	public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First Number: ");
+                int a =sc.nextInt();
+ 	System.out.print("Enter the Second Number: ");
+                int b =sc.nextInt();
+	System.out.print("Enter the Third Number: ");
+                int c =sc.nextInt();
+
+		int average =(a+b+c)/3;
+
+
+        System.out.print("Average ="+ average);
+}
+}
