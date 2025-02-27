779373cfc986158e07f18c78bbbb4d299a20fa49
Samueljain26 <samueljain77@gmail.com>
2025-02-27 12:01:54 +0530

    Calculating Perimiter of Rectangle

---
+++ PerimeterRectangle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class PerimeterRectangle{
+         public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length: ");
+                int l =sc.nextInt();
+ System.out.print("Enter the width: ");
+                int w =sc.nextInt();
+                 int p = 2*(l+w);
+        System.out.print("Perimeter ="+ p);
+}
+}
