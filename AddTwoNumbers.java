8c5a340b66c5f99940debfabdd646467d6d204ed
Samueljain26 <samueljain77@gmail.com>
2025-02-27 11:28:28 +0530

    Sum of two numbers

---
+++ AddTwoNumbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class addTwoNumbers{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter First Number: ");
+		int x= sc.nextInt();
+			System.out.print("Enter Second Number: ");
+ int y=sc.nextInt();
+ System.out.print("sum=" + (x+y));
+}}
